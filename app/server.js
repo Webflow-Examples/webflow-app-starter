0c2546c28c133ae3124003084600db1e34a2b388
John Agan <john.agan@webflow.com>
2022-09-19 06:24:49 -0700

    initial commit

---
+++ app/server.js
@@ -0,0 +1,61 @@
+import Webflow from "webflow-api";
+import App from "./webflow.js";
+import Fastify from "fastify";
+
+// Load environment variables from .env file
+const { CLIENT_ID, CLIENT_SECRET, SERVER_HOST, PORT } = process.env;
+
+// Create a new Webflow App instance
+const app = new App(CLIENT_ID, CLIENT_SECRET);
+
+const server = Fastify({
+  logger: true,
+});
+
+// Response to Webhooks
+server.post("/webhook", async (req, reply) => {
+  // Get site ID from webhook payload
+  const { site } = req.body;
+
+  // Get the site's access token
+  const token = await app.data.get(site);
+
+  // Initialize a new Webflow client
+  const webflow = new Webflow({ token });
+
+  // Make calls to the Webflow API
+  const user = await webflow.get("/user");
+  // Do other stuff with the API...
+
+  // Return a 200 response to Webflow
+  reply.statusCode = 200;
+});
+
+// Install the App
+server.get("/", async (req, reply) => {
+  const { code } = req.query;
+
+  // If a code is passed in, attempt to install the App
+  // othersise, redirect to the install URL to start OAuth
+  if (code) {
+    // install the App and get an access token
+    const token = await app.install(code);
+
+    // add webhook to each site
+    const triggerType = "site_publish";
+    const url = SERVER_HOST + "/webhook";
+    const webhooks = app.addWebhooks(triggerType, url, token);
+
+    return webhooks;
+  } else {
+    // Generate a URL for a user to install the App on Webflow
+    const installUrl = app.installUrl();
+
+    // Redirect the user to the install URL
+    return reply.redirect(installUrl);
+  }
+});
+
+server.listen({ port: PORT, host: "0.0.0.0" }, (err) => {
+  if (err) throw err;
+});
---
+++ app/webflow.js
@@ -0,0 +1,86 @@
+import { AuthorizationCode } from "simple-oauth2";
+import Client from "webflow-api";
+import { Level } from "level";
+
+class App {
+  /**
+   * @param {string} clientId The OAuth client ID for the app
+   * @param {string} clientSecret The OAuth client secret for the app
+   */
+  constructor(clientId, clientSecret) {
+    // KVS for app data
+    this.data = new Level("data");
+
+    // OAuth options
+    this.oauth = new AuthorizationCode({
+      client: {
+        id: clientId,
+        secret: clientSecret,
+      },
+      options: {
+        bodyFormat: "json",
+        authorizationMethod: "body",
+      },
+      auth: {
+        tokenHost: "https://api.webflow.com",
+        authorizeHost: "https://webflow.com",
+      },
+    });
+  }
+
+  /**
+   * Install an App and get the user's access token
+   *
+   * @param {string} code The authorization code used to retrieve an access_token for the user. Can only be used once.
+   *
+   * @returns The user's access token
+   */
+  async install(code) {
+    const access = await this.oauth.getToken({ code });
+    return access.token.access_token;
+  }
+
+  /**
+   * Create Webhooks for all sites available
+   *
+   * @param {string} triggerType The webhook trigger type to listen for
+   * @param {string} url The url to send the webhook events to
+   * @param {string} token The access token to use for creating the webhooks
+   *
+   * @returns An array of webhook created
+   */
+  async addWebhooks(triggerType, url, token) {
+    const client = new Client({ token });
+    const sites = await client.sites();
+    const created = [];
+
+    // create a webhook for each site
+    for (const { _id } of sites) {
+      const args = { triggerType, siteId: _id, url };
+      const webhook = await client.createWebhook(args);
+      created.push(webhook);
+
+      // store token for a site_id to handle webhooks events
+      await this.data.put(webhook.site, token);
+    }
+
+    // Return all webhooks created
+    return created;
+  }
+
+  /**
+   * Generate a URL for a user to install the App on Webflow
+   *
+   * @param params
+   * @param params.redirectURI String representing the registered application URI where the user is redirected after authentication
+   * @param params.scope String or array of strings representing the application privileges
+   * @param params.state String representing an opaque value used by the client to main the state between the request and the callback
+   *
+   * @return The Webflow installation url
+   */
+  installUrl(params = {}) {
+    return this.oauth.authorizeURL(params);
+  }
+}
+
+export default App;
